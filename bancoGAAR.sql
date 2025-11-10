6005eee764de8054a00ed592f13296ab2d82834d
Silkie <arthurjuniokelersilva@gmail.com>
2025-11-10 11:11:32 -0300

    Update bancoGAAR.sql

--- bancoGAAR.sql
+++ bancoGAAR.sql
@@ -1,6 +1,20 @@
+create database bancogaar;
 create table usuarios(
 iduser int primary key auto_increment,
 usuario varchar(50) not null,
 fone varchar(15),
 login varchar(15) not null unique,
 senha varchar(15) not null);
+
+create table clientes(
+idcli int primary key auto_increment,
+nomecli varchar(50) not null,
+endcli varchar(100),
+fonecli varchar(15) not null,
+emailcli varchar(50));
+
+INSERT INTO usuarios(iduser, usuario, fone, login, senha)
+values(' ', ' ', ' ', ' ', ' ');
+
+INSERT INTO clientes(idcli, nomecli, endcli, fonecli, emailcli)
+values(' ', ' ', ' ', ' ', ' ');
